93a5e8c45839434c9304d7ec89b72272b556bce4
ChristianP5 <christiankps111@gmail.com>
2024-05-11 15:43:59 +0700

    fixxed indentations

--- src/routes.js
+++ src/routes.js
@@ -1,47 +1,44 @@
 const {
-    getRootHandler, get404Handler, getRedirectHandler,
-    getStaticHandler, getDownloadHandler, postHomeHandler
+  getRootHandler, get404Handler, getRedirectHandler,
+  getStaticHandler, getDownloadHandler, postHomeHandler,
 } = require('./handlers');
-const path = require('path');
 
 const routes = [
-    {
-        path: '/',
-        method: 'GET',
-        handler: getRootHandler,
-    },
-    {
-        path: '/redirect',
-        method: 'GET',
-        handler: getRedirectHandler,
-    },
-    {
-        path: '/static',
-        method: 'GET',
-        handler: getStaticHandler,
-    },
-    {
-        path: '/download',
-        method: 'GET',
-        handler: getDownloadHandler,
-    },
-    {
-        path: '/{any*}',
-        method: 'GET',
-        handler: get404Handler,
-    },
-    {
-        path: '/css/style.css',
-        method: 'GET',
-        handler: (request, h)=>{
-            return h.file('./css/style.css');
-        },
-    },
-    {
-        path: '/home',
-        method: 'POST',
-        handler: postHomeHandler,
-    },
-]
+  {
+    path: '/',
+    method: 'GET',
+    handler: getRootHandler,
+  },
+  {
+    path: '/redirect',
+    method: 'GET',
+    handler: getRedirectHandler,
+  },
+  {
+    path: '/static',
+    method: 'GET',
+    handler: getStaticHandler,
+  },
+  {
+    path: '/download',
+    method: 'GET',
+    handler: getDownloadHandler,
+  },
+  {
+    path: '/{any*}',
+    method: 'GET',
+    handler: get404Handler,
+  },
+  {
+    path: '/css/style.css',
+    method: 'GET',
+    handler: (request, h) => h.file('./css/style.css'),
+  },
+  {
+    path: '/home',
+    method: 'POST',
+    handler: postHomeHandler,
+  },
+];
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,51 +1,48 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
 const path = require('path');
+const routes = require('./routes');
 
 const init = async () => {
-    const server = Hapi.server({
-        host: process.env.NODE_ENV ==='production' ? '0.0.0.0' : 'localhost',
-        port: 5000,
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-            files: {
-                relativeTo: path.join(__dirname, 'static'),
-            }
-        },
-        
-    });
+  const server = Hapi.server({
+    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
+    port: 5000,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+      files: {
+        relativeTo: path.join(__dirname, 'static'),
+      },
+    },
 
+  });
 
-    await server.register([
-        {
-            plugin: require('@hapi/inert'),
-        },
-        {
-            plugin: require('@hapi/vision'),
-        }
-    ])
+  await server.register([
+    {
+      plugin: require('@hapi/inert'),
+    },
+    {
+      plugin: require('@hapi/vision'),
+    },
+  ]);
 
-    server.route(routes);
+  server.route(routes);
 
-    
-    server.views({
-        engines: {
-            hbs: require('handlebars'),
-        },
-        path: path.join(__dirname, 'views'),
-        layout: 'layout',
-    })
+  server.views({
+    engines: {
+      hbs: require('handlebars'),
+    },
+    path: path.join(__dirname, 'views'),
+    layout: 'layout',
+  });
 
+  await server.start();
+  console.log(`Server started at ${server.info.uri}`);
+};
 
-    await server.start();
-    console.log(`Server started at ${server.info.uri}`);
-}
-
-process.on('unhandledRejection', (error)=>{
-    console.log(error);
-    process.exit(1);
-})
+process.on('unhandledRejection', (error) => {
+  console.log(error);
+  process.exit(1);
+});
 
 init();
